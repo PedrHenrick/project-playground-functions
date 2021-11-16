ba99e73efef6f39b504e24f66986496b7c607ffe
Pedro <phmc@ic.ufal.br>
2021-11-15 22:28:41 -0300

    Algumas mudanças na sintaxe 2

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,72 +1,63 @@
 // Desafio 10
-function techList(array, User) {
+function techList(tecnologias, userName) {
   // seu código aqui
-  // Referência site: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-  let technology = [];
-  let resposta;
-  let object = {};
-  if (array.length === 0) resposta = 'Vazio!';
-  else {
-    for (let index = 0; index < array.length; index += 1) {
-      technology.push(object = { tech: array[index], name: User });
+  // referência site: https://www.youtube.com/watch?v=JxdsTHdgqAU
+  let arrayVazio = [];
+  let objeto = {};
+  if (tecnologias.length === 0) {
+    return 'Vazio!';
+  }
+  if (tecnologias.length > 0) {
+    for (let index = 0; index < tecnologias.length; index += 1) {
+      arrayVazio.push(objeto = { tech: tecnologias[index], name: userName });
     }
-    if (array.length === 5) {
-      technology.sort(function (a, b) {
-        if (a.tech > b.tech) return 1;
-        if (a.tech < b.tech) return -1;
-        return 0; }); resposta = technology 
-    } 
-    else resposta = technology;
-  } return resposta;
+  }
+  if (tecnologias.length >= 5) {
+    const ordem = arrayVazio.sort((a, b) => {
+      return a.tech.localeCompare(b.tech);
+    });
+  }
+  return arrayVazio;
 }
 // console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber(numbers) {
+function generatePhoneNumber(num) {
   // seu código aqui
-  // referência de site: https://www.horadecodar.com.br/2020/11/12/como-fazer-uma-copia-de-array-sem-referencia-em-javascript/
-  let total = numbers.length;
-  let repetido = 1;
-  let numero = numbers.slice();
-  let indexSecond = 0;
-  let string = '';
+  let total = num.length;
+  let repetido = 0;
   if (total !== 11) return 'Array com tamanho incorreto.';
-  else {
-    for (let index = 0; index < total; index += 1) { 
-      if (numbers[index] > 9 || numbers[index] < 0) {
+  if (total === 11) {
+    for (let index = 0; index < total; index += 1) {
+      if (num[index] > 9 || num[index] < 0) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
     }
-    for (let index = 1; index < total; index += 1) {
-      for (let indexTwo = 0; indexTwo < index; indexTwo += 1) {
-        if (numero[index] > numero[indexTwo]) {
-          let aux = numero[index];
-          numero[index] = numero[indexTwo];
-          numero[indexTwo] = aux;
+    for (let index = 0; index < total; index += 1) {
+      for (let secondIndex = 1; secondIndex < total; secondIndex += 1) {
+        if (num[index] === num[secondIndex]) {
+          repetido += 1;
+          if (repetido === 3) return 'não é possível gerar um número de telefone com esses valores';
         }
       }
+      repetido = 0;
     }
-    for (let index = 1; index < total; index += 1) {
-      if (numero[indexSecond] === numero[index]) {
-        repetido += 1;
-        if (repetido >= 3) return 'não é possível gerar um número de telefone com esses valores';
-      } else if (numero[indexSecond] !== numero[index + 1]) indexSecond += 1;
-    }
-    string = '(' + numbers[0] + numbers[1] + ')' + ' ' + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + '-' + numbers[7] + numbers[8] + numbers[9] + numbers[10];
-    return string;
+    
   }
+  return '(' + num[0] + num[1] + ') ' + num[2] + num[3] + num[4] + num[5] + num[6] + '-' + num[7] + num[8] + num[9] + num[10];
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
   // referência site: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
-  let absoluteA = Math.abs(lineA);
-  let absoluteB = Math.abs(lineB);
-  let absoluteC = Math.abs(lineC);
   let resposta;
-  let diferençaAbsoluta = absoluteA - absoluteC - absoluteB;
-  if ((lineA + lineB) > lineC && (lineC + lineB) > lineA && (lineA + lineC) > lineB && lineC > diferençaAbsoluta && lineB > diferençaAbsoluta && lineA > diferençaAbsoluta) resposta = true;
+  let diferençaAbsoluta = Math.abs(lineA) - Math.abs(lineB) - Math.abs(lineC);
+  let somaAB = lineA + lineB;
+  let somaAC = lineA + lineC;
+  let somaCB = lineC + lineB;
+
+  if (somaAB > lineC && somaCB > lineA && somaAC > lineB && lineC > diferençaAbsoluta && lineB > diferençaAbsoluta && lineA > diferençaAbsoluta) resposta = true;
   else resposta = false;
   return resposta;
 }
